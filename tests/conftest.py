46410d415b7375dfe38d1ef043118fdb7108305a
Andrey~ <andrkrestyaninov@gmail.com>
2023-07-11 23:51:18 +0300

    Added fixtures, upgraded the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,14 @@ import json
 
 
 @pytest.fixture(scope="function")
-def get_info(request):
-    """Gets info about person condition and his answers"""
+def answers():
+    """Gets person answers"""
     with open("./json/answers.json", 'r') as file:
-        answers: list = json.load(file)
-    with open("./json/condition.json", 'r') as file:
-        condition: list = json.load(file)
-    request.cls.answers = answers
-    request.cls.condition = condition
+        return json.load(file)
+
     
+@pytest.fixture(scope="function")
+def condition():
+    """Gets info about person condition"""
+    with open("./json/condition.json", 'r') as file:
+        return json.load(file)
--- tests/test_person.py
+++ tests/test_person.py
@@ -8,52 +8,45 @@ Upload information about person from json and check it.
 import pytest
 
 
-@pytest.mark.usefixtures("get_info")
-def test_sex(get_info):
+def test_sex(answers, condition):
     """Checks that people blushing on talks about relationships"""
-    if get_info.answers[5] == '1' or get_info.answers[5] == '2':
-        assert int(get_info.condition[6]["blushing level"]) - \
-            int(get_info.condition[5]["blushing level"]) > 1
+    if answers[5] == '1' or answers[5] == '2':
+        assert int(condition[6]["blushing level"]) - \
+            int(condition[5]["blushing level"]) > 1
 
 
-@pytest.mark.usefixtures("get_info")
-def test_death(get_info):
+def test_death(answers, condition):
     """Checks that old people fear the death"""
-    if get_info.answers[0] == '4':
-        assert int(get_info.condition[1]["heart rate"]) - \
-            int(get_info.condition[0]["heart rate"]) > 5
+    if answers[0] == '4':
+        assert int(condition[1]["heart rate"]) - \
+            int(condition[0]["heart rate"]) > 5
 
 
-@pytest.mark.usefixtures("get_info")
-def test_parents(get_info):
+def test_parents(answers, condition):
     """Checks that people don't live with dead parents"""
-    if get_info.answers[2] == '1':
-        assert get_info.answers[7] != '4'
+    if answers[2] == '1':
+        assert answers[7] != '4'
 
 
-@pytest.mark.usefixtures("get_info")
-def test_work(get_info):
+def test_work(answers, condition):
     """Checks that people who loooking for job don't be richy"""
-    if get_info.answers[3] == '3':
-        assert get_info.answers[4] != '4'
+    if answers[3] == '3':
+        assert answers[4] != '4'
 
 
-@pytest.mark.usefixtures("get_info")
-def test_money(get_info):
+def test_money(answers, condition):
     """Checks that heart of angry people started moving faster"""
-    if get_info.answers[4] == '1':
-        assert int(get_info.condition[4]["heart rate"]) - \
-            int(get_info.condition[3]["heart rate"]) > 10
+    if answers[4] == '1':
+        assert int(condition[4]["heart rate"]) - \
+            int(condition[3]["heart rate"]) > 10
 
 
-@pytest.mark.usefixtures("get_info")
-def test_condition(get_info):
+def test_condition(answers, condition):
     """Checks that people with bad mood don't have a large pupils"""
-    if get_info.answers[9] == '4':
-        assert int(get_info.condition[9]["pupillary dilation"]) < 6
+    if answers[9] == '4':
+        assert int(condition[9]["pupillary dilation"]) < 6
 
 
-@pytest.mark.usefixtures("get_info")
-def test_president(get_info):
+def test_president(answers, condition):
     """Checks that people not worried too much on question about president"""
-    assert int(get_info.condition[8]["heart rate"]) < 125
+    assert int(condition[8]["heart rate"]) < 125
