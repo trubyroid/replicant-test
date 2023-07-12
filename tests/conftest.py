49d3ff10b1d2f24e8e2eddb1d2903f8d56ac0ac5
Andrey~ <andrkrestyaninov@gmail.com>
2023-07-12 13:27:30 +0300

    Fixed comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ import json
 
 @pytest.fixture(scope="function")
 def answers():
-    """Gets person answers"""
+    """Receives person's answers"""
     with open("./json/answers.json", 'r') as file:
         return json.load(file)
 
     
 @pytest.fixture(scope="function")
 def condition():
-    """Gets info about person condition"""
+    """Receives information about a person's condition"""
     with open("./json/condition.json", 'r') as file:
         return json.load(file)
